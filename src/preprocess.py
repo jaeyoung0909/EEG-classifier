fbf3d3bb85be83a8b6ced0ec1f1ff19f28252391
jaeyoung0909 <jaeyoung0909@kaist.ac.kr>
2020-09-13 15:53:33 +0900

    add file converter and train module

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,12 +1,14 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import mne
+import pandas as pd
 
 def csv_load(path):
     data = np.genfromtxt(path, delimiter=',')
     return data
 
-def label_load(path):
+def label2np(path):
     f = open(path, 'r')
     f = f.read().split('\n')
     data = []
@@ -14,25 +16,40 @@ def label_load(path):
         tmp = line.split(',')
         if len(tmp) == 11:
             data.append(tmp[2])
-    return data
+    assert len(data)%5 == 2
+    data = [data[4 + i*5] for i in range(len(data)//5)]
+    def criteria(char):
+        if char == 'W':
+            return 0
+        elif char == 'R':
+            return 1
+        elif char == 'NR':
+            return 2
+        assert False
+    data = [criteria(i) for i in data]
+    return np.array(data)
+
+def edf2csv(path):
+    edf = mne.io.read_raw_edf(path)
+    header = ','.join(edf.ch_names)
+    np.savetxt(path.split('.')[0]+'.csv', edf.get_data().T, delimiter=',', header=header)
 
 def fft(data):
     window = np.hamming(data.size)
     N = window.size
-    return np.square(np.absolute(2.0//N * np.fft.fft(window * data)[:N//2]))
+    return np.square(np.absolute(2.0/N * np.fft.fft(window * data)[:N//2]))
 
 
 def eeg2img(data):
     step = 250
     time = 20
-    time_step = 2
+    time_step = 4
     freq = 2000
     freq_step = time_step * freq
     eeg1 = []
     eeg2 = []
     emg = []
     for i in range(freq * time // step):
-        print("step {}".format(i+1))
         eeg1_row = []
         eeg2_row = []
         for j in range(24):
@@ -42,17 +59,45 @@ def eeg2img(data):
         eeg2.append(eeg2_row)
         emg_sum = sum(fft(data[i*step : freq_step + i*step, 2])[1:30])
         emg.append([emg_sum for x in range(24)])
+    eeg1 = np.array(eeg1)
+    eeg1 = (eeg1 - np.mean(eeg1)) / np.std(eeg1)
+    eeg2 = np.array(eeg2)
+    eeg2 = (eeg2 - np.mean(eeg2)) / np.std(eeg2)
+    emg = np.array(emg)
+    emg = (emg - np.mean(emg)) / np.std(emg)
+    
     arr = np.dstack((eeg1, eeg2, emg))
-    std = (arr - np.mean(arr)) / np.std(arr)
-    return std
+    return arr
 
-data = csv_load("EDF/190605C1_5min.csv")
+def eeg2csv(path_eeg):
+    step = 20 * 2000
+    
+    arr = []
+    for i, chunk in enumerate(pd.read_csv(path_eeg, chunksize=step)):
+        if i+1 > 4320:
+            continue
+        print("step {}".format(i+1))
+        data = chunk.to_numpy()
+        arr.append(eeg2img(data))
+    
+    f = open(path_eeg.split('.')[0] + '_chunk.npy', 'wb')
+    np.save(f, np.array(arr))
+    f.close()
+
+
+
+# data = csv_load("EDF/190605C1_5min.csv")
 # freq = np.fft.fftfreq(4000, 0.0005)
 # y = fft(data[0:4000, 2])
 # plt.plot(freq[:2000], y)
 # print(freq)
 # plt.show()
+# print(eeg2img(data[:2000*20]).shape)
+# labeltxt2csv("EDF/190605C1_5min_Result.txt")
+# print(len(labeltxt2csv("EDF/190605C1_5min_Result.txt")))
 
-# print(eeg2img(data).shape)
-print(len(data)/4000)
-print(len(label_load("EDF/190605C1_5min_Result.txt")))
+# eeg2csv("EDF/190605C1.csv")
+# b = np.load("EDF/190605C1_chunk.npy")
+# print(b.shape)
+# print(len(csv_load("EDF/190605C1.csv"))/20*2000)
+# print(label2np("EDF/190605C1_Hypno.txt").shape)
--- src/train.py
+++ src/train.py
@@ -2,33 +2,52 @@ import os
 
 import torch
 import torch.optim as optim
+import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import DataLoader
-import EEGClassifier from model
+import numpy as np
+from torch.utils.data import TensorDataset, DataLoader
+from model import EEG_Classifier 
+from preprocess import label2np
 
 
 
 torch.manual_seed(470)
 torch.cuda.manual_seed(470)
 
+device = 'cuda'
 max_epoch = 40
 learning_rate = 0.001
 batch_size = 32
 
+eeg_path = "EDF/190605C1_chunk.npy"
+label_path = "EDF/190605C1_Hypno.txt"
+data = np.load(eeg_path)
+B, W, H, C = data.shape
+data = np.reshape(data, (B, C, H, W)) 
+label = label2np(label_path)
+train_x = torch.Tensor(data[:4000])
+train_y = torch.Tensor(label[:4000]).long()
+test_x = torch.Tensor(data[4000:])
+test_y = torch.Tensor(label[4000:]).long()
+train_dataset = TensorDataset(train_x, train_y)
+train_dataloader = DataLoader(train_dataset, batch_size=32)
+test_dataset = TensorDataset(test_x, test_y)
+test_dataloader = DataLoader(test_dataset, batch_size=32)
+
 training_process = True
 
-model = EEGClassifier()
-
+model = EEG_Classifier()
+model = model.to(device)
 def init_weights(m):
     if type(m) == nn.Linear or type(m) == nn.Conv2d:
-        torch.n.ninit.xavier_uniform(m.weight)
+        torch.nn.init.xavier_uniform(m.weight)
 
 model.apply(init_weights)
 
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-ckpt_dir = op.path.join(os.getcwd(), 'checkpoints')
+ckpt_dir = os.path.join(os.getcwd(), 'checkpoints')
 if not os.path.exists(ckpt_dir):
-    os.makedirs(dkpt_dir)
+    os.makedirs(ckpt_dir)
 
 best_acc = 0.
 ckpt_path = os.path.join(ckpt_dir, 'EEGmodel.pt')
@@ -56,16 +75,16 @@ if training_process:
             labels = labels.to(device)
 
             logits = model(inputs)
-
+            
             loss = F.cross_entropy(logits, labels)
 
             optimizer.zero_grad()
-            loss.backwrad()
+            loss.backward()
             optimizer.step()
             acc = (logits.argmax(dim=1) == labels).float().mean()
 
-            if it % 2000 == 0:
-                print('[epoch:{}, iteration:{}] train loacc : {:.4f} train accuracy: {:.4f}'.format(epoch, it, loss,item(), acc.item()))
+            if it % 200 == 0:
+                print('[epoch:{}, iteration:{}] train loacc : {:.4f} train accuracy: {:.4f}'.format(epoch+1, it, loss.item(), acc.item()))
         
         train_losses.append(loss)
 
@@ -76,20 +95,20 @@ if training_process:
         for test_inputs, test_labels in test_dataloader:
             test_inputs = test_inputs.to(device)
             test_labels = test_labels.to(device)
-            lotis = model(test_inputs)
+            logits = model(test_inputs)
             test_loss += F.cross_entropy(logits, test_labels, reduction='sum')
-            test_acc += (logits.armax(dim=1) == test_labels).float().sum().item()
+            test_acc += (logits.argmax(dim=1) == test_labels).float().sum().item()
             n += test_inputs.size(0)
         test_loss /= n
         test_acc /= n 
         test_losses.append(test_loss)
-        print('[epoch:{}, iteration:{}] test_loss : {:.4f} test accuracy : {:.4f}'.format(epoch, it, test_loss, test_acc)) 
+        print('[epoch:{}, iteration:{}] test_loss : {:.4f} test accuracy : {:.4f}'.format(epoch+1, it, test_loss, test_acc)) 
 
         # save checkpoint whenever there is improvement in performance
         if test_acc > best_acc:
             best_acc = test_acc
             # Note: optimizer also has states ! don't forget to save them as well.
-            ckpt = {'my_classifier':my_classifier.state_dict(),
+            ckpt = {'model':model.state_dict(),
                     'optimizer':optimizer.state_dict(),
                     'best_acc':best_acc}
             torch.save(ckpt, ckpt_path)
